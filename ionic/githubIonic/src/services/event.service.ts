d1123e5a76da3059a5e60284732003c4eb5319ef
Elijah Balogun <jah.balo16@gmail.com>
2017-06-11 14:22:10 -0400

    added getEvent methods

--- ionic/githubIonic/src/services/event.service.ts
+++ ionic/githubIonic/src/services/event.service.ts
@@ -1,9 +1,9 @@
 //imports
+import { User } from './../model/user';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 //import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import { Http, Response, Headers } from '@angular/http';
-import { User } from '../src/Model/user';
+import { Http, Response, Headers,RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -11,6 +11,7 @@ import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class EventService {
+  
     // Resolve HTTP using the constructor
     constructor(private http: Http, private authService: AuthService) { }
     // private instance variable to hold base url
@@ -19,20 +20,18 @@ export class EventService {
 
 // post event to server
 createEvent(title, time, endTime, venue): Observable<any> {
-       
+           //pass the parameter to the data properties
             let data = {
                 "title": title,
                 "time": time,
                 "endTime": endTime,
                 "venue": venue
             }
-            //place data in readable JSON format for the server
+            //place data object in readable JSON format to be sent to the server
             let body = JSON.stringify(data); 
-            let head = new Headers({
-                'Content-Type': 'application/json',
-                    });
+           
 
-return this.http.post(this.Url,body, {headers: head})
+return this.http.post(this.Url,body, this.auth())
         // call json to the response object
         .map((res) => res.json())
         // handle errors if any
@@ -42,11 +41,33 @@ return this.http.post(this.Url,body, {headers: head})
     
 
 // method to get all events created by a user
-    getUserEvents() {
 
+   getUserEvents(){
+     return this.http.get(this.Url , this.auth())
+     .map((res: Response) => res.json());
+   
+   }
+  
+    getAllEvents() {
+    // specify the content header
+            let head = new Headers({
+                'Content-Type': 'application/json',
+                    });
+                     return this.http.get(this.Url , head)
+     .map((res: Response) => res.json());
     }
 
     
  
-   
+   private auth() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token,
+            'Content-Type': 'application/json', });
+            return new RequestOptions({ headers: headers });
+        }
+    }
 }
+
+
