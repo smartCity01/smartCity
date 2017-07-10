47354a53df300e873c2c68c24360e68b85ef6bc4
Korede Ogunyinka <koredejust4u@live.com>
2017-07-09 21:38:45 -0400

    add autocomplete to the venue

--- ionic/githubIonic/src/pages/locations/locations.ts
+++ ionic/githubIonic/src/pages/locations/locations.ts
@@ -1,20 +1,150 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 
+declare var google;
+
 @Component({
-  selector: 'locations',
+  selector: 'locations-page',
   templateUrl: 'locations.html'
 })
 export class LocationsPage {
-  lat: number;
-  lng: number;
-  constructor() {
-    Geolocation.getCurrentPosition().then(pos => {
-      this.lat = pos.coords.latitude;
-      this.lng = pos.coords.longitude;
-    }),
-      err => {
-        //do nothing
-      }
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController) {
   }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  loadMap() {
+
+    Geolocation.getCurrentPosition().then((position) => {
+
+      console.log(position.coords.latitude, position.coords.longitude);
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+          { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{ color: '#263c3f' }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#6b9a76' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{ color: '#38414e' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#212a37' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#9ca5b3' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{ color: '#746855' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#1f2835' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#f3d19c' }]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{ color: '#2f3948' }]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#17263c' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#515c6d' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{ color: '#17263c' }]
+          }
+        ]
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
 }
--- ionic/githubIonic/src/pages/new-event/new-event.ts
+++ ionic/githubIonic/src/pages/new-event/new-event.ts
@@ -1,9 +1,10 @@
 import { RefresherService } from './../../services/refresher.service';
 import { EventService } from './../../services/event.service';
 //import { EventDetailsPage } from './../event-details/event-details';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { ViewController, ModalController, ToastController } from 'ionic-angular';
 
+declare var google;
 
 @Component({
   selector: 'new-event',
@@ -11,23 +12,44 @@ import { ViewController, ModalController, ToastController } from 'ionic-angular'
 })
 
 export class NewEventPage {
+  @ViewChild('display', { read: ElementRef }) inputElement: ElementRef;
   title: String;
   time: number;
   endTime: number;
   venue: String;
   description: String;
-  success = "Event was created successfully";
-  error = "Error! unable to create event";
+  geocoder = new google.maps.Geocoder();
+  service = new google.maps.places.AutocompleteService();
+  predictions;
+
+
   constructor(
     public eventService: EventService,
     public viewCtrl: ViewController,
     public modalCtrl: ModalController,
     public toastCtrl: ToastController,
-    private refresherService: RefresherService
+    private refresherService: RefresherService,
+    private cd: ChangeDetectorRef
   ) { }
+
+
+  onInputPress(event) {
+    if (!event.target.value) {
+      this.predictions = null;
+    }
+    let value = event.target.value;
+    if (value) {
+      this.service.getPlacePredictions({ input: event.target.value }, (predictions, status) => {
+        this.predictions = predictions;
+        this.cd.detectChanges();
+      });
+    }
+  }
+
 
   //method to create event
   create() {
+    console.log(this.venue);
     this.eventService.createEvent(this.title, this.time, this.endTime, this.venue, this.description).subscribe(res => {
       this.viewCtrl.dismiss();
       let toast = this.toastCtrl.create({
@@ -39,9 +61,21 @@ export class NewEventPage {
       this.refresherService.refresh();
     },
       err => {
-        console.log(this.error);
+        console.log('failed to create event');
       }
     );
+  }
+
+  clickedLocation(prediction) {
+    this.venue = prediction.description;
+    this.predictions = null;
+    this.geocoder.geocode({ 'placeId': prediction.place_id }, (responses, status) => {
+      if (status == 'OK') {
+        var lat = responses[0].geometry.location.lat();
+        var lng = responses[0].geometry.location.lng();
+        console.log(lat, lng);
+      }
+    });
   }
 
 
